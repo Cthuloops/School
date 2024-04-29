b9ae75e9f9876f9f0498a558d410b265f218e507
hcoughlin <104121493+Cthuloops@users.noreply.github.com>
2024-04-29 11:27:51 -0400

    some more work being done

--- FTCC/Spring2024/CSC121/M7/m7_project.py
+++ FTCC/Spring2024/CSC121/M7/m7_project.py
@@ -5,11 +5,43 @@
 
 # let's make this class I guess
 class Student():
+    """A class to reprsent a Student
 
-    def __init__(self, stu_id, first_name, last_name, major, courses, email):
-        """A class to reprsent a Student
+    Attributes
+    ----------
+    stu_id : int
+        student ID number
+    first_name : str
+        first name of the student
+    last_name : str
+        last name of the student
+    major : str
+        the students major
+    courses : List[str]
+        list of courses the student is enrolled in
+    email : str
+        email for the student
 
-        Attributes
+    Methods
+    -------
+    get_stu_id():
+        returns the student's id
+    get_first_name():
+        returns the student's first name
+    get_last_name():
+        returns the student's last name
+    get_major():
+        returns the student's major
+    get_courses():
+        returns the student's courses
+    get_email():
+        returns the student's email
+    """
+
+    def __init__(self, stu_id, first_name, last_name, major, courses):
+        """Constructs all the necessary attributes for the Student object
+
+        Parameters
         ----------
         stu_id : int
             student ID number
@@ -21,8 +53,6 @@ class Student():
             the students major
         courses : List[str]
             list of courses the student is enrolled in
-        email : str
-            email for the student
         """
 
         self.__stu_id = stu_id
@@ -30,18 +60,28 @@ class Student():
         self.__last_name = last_name
         self.__major = major
         self.__courses = courses
-        self.__email = email
+        self.__email = Student.set_email(self, stu_id, last_name)
 
     def __repr__(self, delim='\n'):
 
         return "ID: " + str(self.__stu_id) + delim + "First name: " + self.__first_name + delim + "Last name: " + self.__last_name + delim + "Major: " + self.__major + delim + "Email: " + self.__email
 
-    def set_email(self, __stu_id, __last_name, atdomext="@student.faytechcc.edu"):
+    def set_email(self, __stu_id, __last_name, domain_ext="student.faytechcc.edu"):
         """Sets an email address for the Student
 
-            Returns
+        If the argument 'atdomext' is passed, then the 
+
+        Parameters
+        ----------
+        domain_ext : str, optional
+            Sets the "domain.extenstion" of the email (default is "student.faytechcc.edu")
+
+        Returns
+        -------
+        email (str) : email generated for the student
         """
-        return self.__stu_id[-2:] + self.__last_name + atdomext
+        email = self.__stu_id[-2:] + self.__last_name.lower() + '@' + domain_ext
+        return email
 
     def get_stu_id(self):
         return self.__stu_id
